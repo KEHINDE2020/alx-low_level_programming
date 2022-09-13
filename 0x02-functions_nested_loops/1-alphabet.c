fda9d78dc41fbe8784af552a1579c77d8048de6f
KEHINDE2020 <kennymoses2489@gmail.com>
2022-09-13 10:55:36 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-*print_alphabet -  entry point.
-Return : 0 (if successful)
+*print_alphabet -  entry point. Return : 0 (if successful)
 */
 void print_alphabet(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
+
 {
 	_putchar(ch);
 }
