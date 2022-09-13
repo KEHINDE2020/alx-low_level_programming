6556958668dc2ec228ed9fe7c11bf8edbeef3519
KEHINDE2020 <kennymoses2489@gmail.com>
2022-09-13 09:45:55 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /*
  * File: 0-putchar.c
  * Auth: Brennan D Baraban
@@ -13,6 +15,7 @@
  */
 int main(void)
 {
+	_putchar('_')
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
