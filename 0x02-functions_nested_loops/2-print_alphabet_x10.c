6922cbef94322c73ec0b3bdf53be59f032c5c9fb
KEHINDE2020 <kennymoses2489@gmail.com>
2022-09-13 11:26:47 -0700

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,15 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int j;
+	int x;
 
-	while (count++ <= 9)
+	for (x = 0; x < 10; x++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
 		_putchar('\n');
 	}
 }
