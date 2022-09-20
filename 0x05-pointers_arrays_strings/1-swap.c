e3c39b4cc65438fd342f945f902fb1b823ec6b82
KEHINDE2020 <kennymoses2489@gmail.com>
2022-09-20 09:13:07 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
