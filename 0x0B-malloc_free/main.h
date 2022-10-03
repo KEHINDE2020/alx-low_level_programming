c46b384626b22e8c190f131a3648fa2e469d4094
KEHINDE2020 <kennymoses2489@gmail.com>
2022-10-03 09:15:07 -0700

    hot fix

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,14 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
 
+/**
+ *  * _putchar - writes the character c to stdout
+ *   * @c: The int to print
+ *    * Return: 0
+ */
+
+int _putchar(char);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
